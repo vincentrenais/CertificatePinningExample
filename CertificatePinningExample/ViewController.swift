32597531d3e6e888c9ef6bf8582938fe7d9178f8
Vincent Renais <vincentrenais@gmail.com>
2017-08-10 16:36:50 -0400

    Initial Commit

---
+++ CertificatePinningExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CertificatePinningExample
+//
+//  Created by Vincent Renais on 8/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
