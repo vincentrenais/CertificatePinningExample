130600ffa8cef5b50d5667af62a7e59916a36e1e
Vincent Renais <vincentrenais@gmail.com>
2017-08-11 11:37:15 -0400

    Add Alamofire and webview

--- CertificatePinningExample/ViewController.swift
+++ CertificatePinningExample/ViewController.swift
@@ -1,24 +1,47 @@
-//
-//  ViewController.swift
-//  CertificatePinningExample
-//
-//  Created by Vincent Renais on 8/10/17.
-//
-
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+
+    private var sessionManager: SessionManager?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let url = URL(string: "https://www.apple.com")!
+        enableCertificatePinning()
+        sessionManager?.request(url).responseData { result in
+        if let data = result.data, let requestURL = result.request?.url {
+            self.webView.load(data,
+                              mimeType: result.response?.mimeType ?? "",
+                              textEncodingName: result.response?.textEncodingName ?? "",
+                              baseURL: requestURL)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func enableCertificatePinning() {
+        let certificates = getCertificates()
+        let trustPolicy = ServerTrustPolicy.pinCertificates(
+            certificates: certificates,
+            validateCertificateChain: true,
+            validateHost: true)
+        let trustPolicies = [ "www.apple.com": trustPolicy ]
+        let policyManager = ServerTrustPolicyManager(policies: trustPolicies)
+        sessionManager = SessionManager(
+            configuration: .default,
+            serverTrustPolicyManager: policyManager
+        )
     }
 
+    private func getCertificates() -> [SecCertificate] {
+        let url = Bundle.main.url(forResource: "appleCert", withExtension: "cer")!
+        let localCertificate = try! Data(contentsOf: url) as CFData
+        guard let certificate = SecCertificateCreateWithData(nil, localCertificate)
+            else { return [] }
 
+        return [certificate]
+    }
 }
-
